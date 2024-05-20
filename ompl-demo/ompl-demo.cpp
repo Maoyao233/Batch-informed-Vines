4415e2599c5f703981a88e2697a6e3e5a99bcfa5
Freyja923 <1142384432@qq.com>
2024-05-21 01:26:24 +0800

    BIVstar: initial commit(BITstar only)

--- ompl-demo/ompl-demo.cpp
+++ ompl-demo/ompl-demo.cpp
@@ -1,84 +1,126 @@
-#include <ompl/base/SpaceInformation.h>
-#include <ompl/base/spaces/SE3StateSpace.h>
+/*********************************************************************
+* Software License Agreement (BSD License)
+*
+*  Copyright (c) 2013, Rice University
+*  All rights reserved.
+*
+*  Redistribution and use in source and binary forms, with or without
+*  modification, are permitted provided that the following conditions
+*  are met:
+*
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above
+*     copyright notice, this list of conditions and the following
+*     disclaimer in the documentation and/or other materials provided
+*     with the distribution.
+*   * Neither the name of the Rice University nor the names of its
+*     contributors may be used to endorse or promote products derived
+*     from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+*  POSSIBILITY OF SUCH DAMAGE.
+*********************************************************************/
+
+/* Author: Bryant Gipson, Mark Moll */
+
+#include <ompl/base/spaces/RealVectorStateSpace.h>
 #include <ompl/geometric/planners/rrt/RRTstar.h>
-#include <ompl/geometric/SimpleSetup.h>
+#include <ompl/geometric/planners/informedtrees/BITstar.h>
+#include <ompl/geometric/planners/informedtrees/AITstar.h>
+#include <ompl/geometric/planners/kpiece/KPIECE1.h>
+#include <ompl/geometric/planners/est/EST.h>
+#include <ompl/geometric/planners/prm/PRM.h>
+#include <ompl/geometric/planners/stride/STRIDE.h>
+#include <ompl/tools/benchmark/Benchmark.h>
+#include <ompl/util/String.h>
 
-#include <ompl/config.h>
-#include <iostream>
-#include <fstream>
+#include <BIVstar.h>
 
-namespace ob = ompl::base;
-namespace og = ompl::geometric;
+#include <boost/math/constants/constants.hpp>
+#include <boost/format.hpp>
 
-bool isStateValid(const ob::State *state) {
-  // cast the abstract state type to the type we expect
-  const auto *se3state = state->as<ob::SE3StateSpace::StateType>();
+unsigned ndim = 6;
+const double edgeWidth = 0.1;
 
-  // extract the first component of the state and cast it to what we expect
-  const auto *pos = se3state->as<ob::RealVectorStateSpace::StateType>(0);
+// Only states near some edges of a hypercube are valid. The valid edges form a
+// narrow passage from (0,...,0) to (1,...,1). A state s is valid if there exists
+// a k s.t. (a) 0<=s[k]<=1, (b) for all i<k s[i]<=edgeWidth, and (c) for all i>k
+// s[i]>=1-edgewidth.
+bool isStateValid(const ompl::base::State *state)
+{
+    const auto& s
+        = *static_cast<const ompl::base::RealVectorStateSpace::StateType*>(state);
+    bool foundMaxDim = false;
 
-  // extract the second component of the state and cast it to what we expect
-  const auto *rot = se3state->as<ob::SO3StateSpace::StateType>(1);
-
-  // check validity of state defined by pos & rot
-
-  // return a value that is always true but uses the two variables we define, so
-  // we avoid compiler warnings
-  return (const void *)rot != (const void *)pos;
+    for (int i = ndim - 1; i >= 0; i--)
+        if (!foundMaxDim)
+        {
+            if (s[i] > edgeWidth)
+                foundMaxDim = true;
+        }
+        else if (s[i] < (1. - edgeWidth))
+            return false;
+    return true;
 }
 
-void plan() {
-  // construct the state space we are planning in
-  auto space{std::make_shared<ob::SE3StateSpace>()};
-
-  // set the bounds for the R^3 part of SE(3)
-  ob::RealVectorBounds bounds(3);
-  bounds.setLow(-10);
-  bounds.setHigh(10);
-
-  space->setBounds(bounds);
-
-  // define a simple setup class
-  og::SimpleSetup ss(space);
-
-  // set state validity checking for this space
-  ss.setStateValidityChecker(
-      [](const ob::State *state) { return isStateValid(state); });
-
-  // create a random start state
-  ob::ScopedState<> start(space);
-  start.random();
-
-  // create a random goal state
-  ob::ScopedState<> goal(space);
-  goal.random();
-
-  // set the start and goal states
-  ss.setStartAndGoalStates(start, goal);
-
-  ss.setPlanner(std::make_shared<og::RRTstar>(ss.getSpaceInformation()));
-
-  // this call is optional, but we put it in to get more output information
-  ss.setup();
-  ss.print();
-
-  // attempt to solve the problem within one second of planning time
-  ob::PlannerStatus solved = ss.solve(10);
-
-  if (solved) {
-    std::cout << "Solutions printed to path.txt" << std::endl;
-    // print the path to screen
-    // ss.simplifySolution();
-    std::ofstream fout{"path.txt"};
-    ss.getSolutionPath().printAsMatrix(fout);
-  } else
-    std::cout << "No solution found" << std::endl;
+void addPlanner(ompl::tools::Benchmark& benchmark, const ompl::base::PlannerPtr& planner, double range)
+{
+    ompl::base::ParamSet& params = planner->params();
+    if (params.hasParam(std::string("range")))
+        params.setParam(std::string("range"), ompl::toString(range));
+    benchmark.addPlanner(planner);
 }
 
-int main(int /*argc*/, char ** /*argv*/) {
-  std::cout << "OMPL version: " << OMPL_VERSION << std::endl;
+int main(int argc, char **argv)
+{
+    if (argc > 1)
+        ndim = std::stoul(argv[1]);
 
-  plan();
+    double range = edgeWidth * 0.5;
+    auto space(std::make_shared<ompl::base::RealVectorStateSpace>(ndim));
+    ompl::base::RealVectorBounds bounds(ndim);
+    ompl::geometric::SimpleSetup ss(space);
+    ompl::base::ScopedState<> start(space), goal(space);
 
-  return 0;
+    bounds.setLow(0.);
+    bounds.setHigh(1.);
+    space->setBounds(bounds);
+    ss.setStateValidityChecker(&isStateValid);
+    ss.getSpaceInformation()->setStateValidityCheckingResolution(0.001);
+    for(unsigned int i = 0; i < ndim; ++i)
+    {
+        start[i] = 0.;
+        goal[i] = 1.;
+    }
+    ss.setStartAndGoalStates(start, goal);
+
+    // by default, use the Benchmark class
+    double runtime_limit = 10, memory_limit = 4096;
+    int run_count = 20;
+    ompl::tools::Benchmark::Request request(runtime_limit, memory_limit, run_count);
+    ompl::tools::Benchmark b(ss, "HyperCube");
+    b.addExperimentParameter("num_dims", "INTEGER", std::to_string(ndim));
+
+    //addPlanner(b, std::make_shared<ompl::geometric::STRIDE>(ss.getSpaceInformation()), range);
+    //addPlanner(b, std::make_shared<ompl::geometric::EST>(ss.getSpaceInformation()), range);
+    //addPlanner(b, std::make_shared<ompl::geometric::KPIECE1>(ss.getSpaceInformation()), range);
+    addPlanner(b, std::make_shared<ompl::geometric::RRTstar>(ss.getSpaceInformation()), range);
+    addPlanner(b, std::make_shared<ompl::geometric::BITstar>(ss.getSpaceInformation()), range);
+    addPlanner(b, std::make_shared<ompl::geometric::AITstar>(ss.getSpaceInformation()), range);
+    addPlanner(b, std::make_shared<ompl::geometric::BIVstar>(ss.getSpaceInformation()), range);
+    b.benchmark(request);
+    b.saveResultsToFile(boost::str(boost::format("hypercube_%i.log") % ndim).c_str());
+
+    return 0;
 }
